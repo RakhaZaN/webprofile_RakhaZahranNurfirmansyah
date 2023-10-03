2e8dac13ff11aa7bb8711abd8845aa477b388fdd
rakhazan <rakhazahnur26@gmail.com>
2023-10-03 08:44:35 +0700

    :sparkles: added some interaction and animation | :lipstick: adjust responsive styling

--- C_Morning_RakhaZahranNurfirmansyah/assets/js/script..js
+++ C_Morning_RakhaZahranNurfirmansyah/assets/js/script..js
@@ -1,8 +1,45 @@
+// Responsive Navbar
+const navToggle = document.getElementById('menuToggle');
+const navCollapse = document.getElementById('itemsCollapse');
+
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('active');
+    navCollapse.classList.toggle('show');
+});
+
+// Navbar Background
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', function () {
+    if (this.window.scrollY > 100) navbar.classList.remove('transparent')
+    else navbar.classList.add('transparent')
+})
+
+// Activate nav-link
+const navLinks = document.querySelectorAll('.nav-link');
+
+function activeSection() {
+    const scrollPosition = window.scrollY;
+
+    navLinks.forEach(link => {
+        const section = document.querySelector(link.getAttribute('href'));
+        const sectionTop = section.offsetTop - 200;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', activeSection);
+
 // Typing effect
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Frontend Web Developer", "UI/UX Designer"];
+const textArray = ["Web Developer", "UI/UX Designer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
